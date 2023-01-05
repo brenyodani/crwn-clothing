f4ba69e1168d70069f78abd98935a0b15eee45f4
brenyodani <77223644+brenyodani@users.noreply.github.com>
2023-01-05 11:47:15 +0100

    changes

--- src/routes/sign-in/sign-in.component.jsx
+++ src/routes/sign-in/sign-in.component.jsx
@@ -1,21 +1,23 @@
-import { signInWithGooglePopup, createUserDocumentFromAuth } from '../../utils/firebase/firebase.utils';
+import {
+    signInWithGooglePopup,
+    createUserDocumentFromAuth,
+  } from '../../utils/firebase/firebase.utils';
 
+import  SignUpForm from '../../components/sign-up-form/sign-up-form.component';
 
-const SignIn = () => {
-    
+  const SignIn = () => {
     const logGoogleUser = async () => {
-        const { user } = await signInWithGooglePopup();
-        const userDocRef = createUserDocumentFromAuth(user);
-        console.log(user);
+      const { user } = await signInWithGooglePopup();
+      const userDocRef = await createUserDocumentFromAuth(user);
     };
-    
+  
     return (
-        <div>
-            <h1>Sign In</h1>
-            <button onClick={logGoogleUser}>Sign in with Google Popup</button>
-        </div>
+      <div>
+        <h1>Sign In Page</h1>
+        <button onClick={logGoogleUser}>Sign in with Google Popup</button>
+        <SignUpForm />
+      </div>
     );
-};
-
-
-export default SignIn;
+  };
+  
+  export default SignIn;
