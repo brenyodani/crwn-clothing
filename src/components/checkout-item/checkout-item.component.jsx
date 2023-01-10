2a5d504615173fb7fc1d889a822888c6c9412c78
brenyodani <77223644+brenyodani@users.noreply.github.com>
2023-01-10 16:16:29 +0100

    checkout page final

--- src/components/checkout-item/checkout-item.component.jsx
+++ src/components/checkout-item/checkout-item.component.jsx
@@ -7,8 +7,7 @@ import './checkout-item.styles.scss';
 const CheckoutItem = ({ cartItem }) => {
   const { name, imageUrl, price, quantity } = cartItem;
 
-  const { clearItemFromCart, addItemToCart, removeItemToCart } =
-    useContext(CartContext);
+  const { clearItemFromCart, addItemToCart, removeItemToCart } = useContext(CartContext);
 
   const clearItemHandler = () => clearItemFromCart(cartItem);
   const addItemHandler = () => addItemToCart(cartItem);
