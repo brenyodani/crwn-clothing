271d53d86a857a98b139774c838dead63e70c496
brenyodani <77223644+brenyodani@users.noreply.github.com>
2023-01-13 16:09:37 +0100

    added some font size chanes

--- src/components/product-card/product-card.component.jsx
+++ src/components/product-card/product-card.component.jsx
@@ -19,7 +19,7 @@ return(
         <img src={ imageUrl } alt={`${ name }`} />
         <div className="footer">
             <span className="name">{ name } </span>
-            <span className="price">{ price } </span>
+            <span className="price">{ price }$</span>
         </div>
         <Button buttonType={ BUTTON_TYPES_CLASSES.inverted } onClick={ addProductToCart }>Add to Cart</Button>
     </div>
